daaa7de7711ede49eb13aca3c212a79547509915
Dell <ak0175200@>
2023-01-25 19:02:08 -0800

    variable class

---
+++ src/variable2/Demovariable.java
@@ -0,0 +1,27 @@
+package variable2;
+
+public class Demovariable {
+    public static void main(String[] args) {
+        String cityName;
+        cityName = "Tundla";
+        System.out.println(cityName);
+        System.out.println("city" + cityName);
+
+
+        int age;
+        age = 28;
+
+        System.out.println("age");
+        System.out.println("Age is :" + age);
+
+
+        double price;
+        price=550.50;
+        System.out.println("price:"+price);
+
+
+
+
+    }
+
+}
---
+++ src/variable2/Demovariable2.java
@@ -0,0 +1,27 @@
+package variable2;
+
+public class Demovariable2 {
+    public static void main(String[] args) {
+        String bikeName="Yamaha R15";
+
+        System.out.println(bikeName);
+        System.out.println("My first bike is :"+bikeName);
+
+
+        int serial_number;
+        serial_number =10687876;
+
+        System.out.println(serial_number);
+        System.out.println("Serialnumber:"+serial_number);
+
+
+        double salary;
+         salary=50000;
+
+        System.out.println("salary");
+        System.out.println("salary:"+salary);
+
+
+
+    }
+        }
