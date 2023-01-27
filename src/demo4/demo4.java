c21f657a0966998312ea160061842d4276de5c04
Dell <ak0175200@>
2023-01-26 17:39:56 -0800

    demo class 4

---
+++ src/demo4/demo4.java
@@ -0,0 +1,28 @@
+package demo4;
+
+import javax.naming.Name;
+
+public class demo4 {
+
+    public static void main(String[] args) {
+
+        String cityName="firozabad";
+        cityName="firzabad";
+        System.out.println(cityName);
+        System.out.println("city" +cityName);
+
+        int age;
+        age= 50;
+        System.out.println("age");
+        System.out.println("age" +age);
+
+
+        double plus;
+        plus = 100;
+        System.out.println("plus" +plus);
+
+
+
+
+    }
+}
