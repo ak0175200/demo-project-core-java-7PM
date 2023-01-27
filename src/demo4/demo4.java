7f3492e7762ca9095de7307002779649fa2d34c9
Dell <ak0175200@>
2023-01-26 23:56:01 -0800

    demo class 4 changes

--- src/demo4/demo4.java
+++ src/demo4/demo4.java
@@ -7,19 +7,48 @@ public class demo4 {
     public static void main(String[] args) {
 
         String cityName="firozabad";
-        cityName="firzabad";
+        cityName="firozabad";
         System.out.println(cityName);
-        System.out.println("city" +cityName);
+        System.out.println("my city name is " +cityName);
 
         int age;
         age= 50;
         System.out.println("age");
-        System.out.println("age" +age);
+        System.out.println("my age  is " +age);
 
 
-        double plus;
-        plus = 100;
-        System.out.println("plus" +plus);
+         int a=10;
+         int b=20;
+         int output= a+b;
+        System.out.println("The addition of a and b is :"+output);
+
+
+        int c=50;
+        int d=20;
+        int out=c-d;
+        System.out.println("The addition of c and d is :" +output);
+
+
+
+        String carName="swift";
+        carName="swift";
+        System.out.println("carname");
+        System.out.println("my car name is "  +carName );
+
+
+        int salary;
+        salary=25000;
+        System.out.println("salary");
+        System.out.println("my salary is "  +salary);
+
+
+
+
+
+
+
+
+
 
 
 
