ae1918cb28182ce68acabf4219327c2dffc41a7e
Dell <ak0175200@>
2023-01-28 19:04:15 -0800

    Commit to Arman Branch

---
+++ src/conditional_statement/Demoif.java
@@ -0,0 +1,51 @@
+package conditional_statement;
+
+public class Demoif {
+
+    public static void main(String[] args) {
+
+    int age;
+    age=20;
+
+    if(age>19){
+
+        System.out.println("person is child"  );
+    }
+
+
+    if(age>45){
+
+        System.out.println("person is young "  );
+    }
+
+
+    int marks;
+    marks=30;
+
+    if(marks<33){
+
+        System.out.println("some students are fail" +marks);
+    }
+
+    int mark;
+    mark=55;
+
+    if(mark>55){
+
+        System.out.println("some student are second division" );
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
